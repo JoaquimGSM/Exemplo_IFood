f2c65b261751f9a21168b6425d5236d09ba12a2c
JoaquimGSM <joaquimgsm1112@gmail.com>
2026-09-17 09:02:26 -0300

    feat: Codificando e entendo o FILTER E REDUCE

--- src/components/Pedido.jsx
+++ src/components/Pedido.jsx
@@ -34,6 +34,13 @@ const pedido = () => {
         )
     }
 
+    // FILTER: Seleciona apenas os produtos disponíveis e do carrinho
+    const produtoDisponiveis = items.filter(item =>item.disponivel);
+    const carrinho = items.filter(item.quantidade >0);
+
+    // REDUCE: Calcula a soma dos items (preço * quantidade) e adiciona a taxa de entrega
+    const subTotal = carrinho.reduce((ac,item)=>ac.item.preco * item.quantidade,0);
+    const total = subTotal >0 ? subTotal + taxaEntrega: 0
 
   return (
     <>
